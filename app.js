3f38cbb82b0bc4eaa98acb9ef9459fe20d54ef13
sugimoto-ecn <kein@encinc.com>
2021-10-29 01:33:13 +0900

    test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ connection.connect(function(err) {
 
 app.get('/' , (req , res) => {
   res.json({
-    message:"this is tomomin api app ; ;"
+    message:"this is tomomin api app ；；"
   })
 })
 app.use('/v1/', router)
