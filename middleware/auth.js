b59c28bad444210062e72b6418f8c5a01e7953d6
sugimoto-ecn <kein@encinc.com>
2021-12-01 02:31:19 +0900

    set alerm api

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,21 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/jwt.config");
+const jwtDecode = require("jwt-decode");
 
 const verifyToken = (req, res, next) => {
   // splitで半角スペースで分割して後ろ側がTokenになる
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(" ")[1];
+  if(req?.params?.userId){
+    const decodedToken = jwtDecode(token);
+    if(decodedToken.id != req.params.userId){
+      res.status(403).send({
+        isSuccess: false,
+        message: "ユーザー認証に失敗しました",
+      });
+    }
+  
+  }
 
   if (token) {
     jwt.verify(token, config.jwt.secret, function (error, decoded) {
